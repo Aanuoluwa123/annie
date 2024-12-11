85b8ec3693a5ff16ca0d4febf87df76dcde8be35
Aanuoluwa123 <ashiyanbiaanu2020@gmail.com>
2024-12-11 22:39:53 +0100

    Add files via upload

---
+++ jspractice.js
@@ -0,0 +1,8 @@
+
+function myproject() {
+    document.getElementById("demo").innerHTML = "Content is now changed! Congratulations."
+}
+
+document.getElementById("demo").innerHTML =5 + 6;
+
+document.write(5 + 6);
